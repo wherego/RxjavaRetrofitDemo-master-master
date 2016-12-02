c00aad234f2ebd1c2f3103ea09bf0fed3c952ec6
dongdongge <625324231@qq.com>
2016-12-02 13:06:44 +0800

    修改配置文件

--- app/src/main/java/com/example/retrofit/retrofit_rx/http/HttpService.java
+++ app/src/main/java/com/example/retrofit/retrofit_rx/http/HttpService.java
@@ -44,8 +44,6 @@ public interface HttpService {
     /*上传文件*/
     @Multipart
     @POST("AppYuFaKu/uploadHeadImg")
-    Observable<BaseResultEntity<UploadResulte>> uploadImage(@Part("uid") RequestBody uid, @Part("auth_key") RequestBody  auth_key,
-                                                            @Part MultipartBody.Part file);
-
+    Observable<BaseResultEntity<UploadResulte>> uploadImage(@Part("uid") RequestBody uid, @Part("auth_key") RequestBody  auth_key, @Part MultipartBody.Part file);
 
 }
